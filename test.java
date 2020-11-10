a7a3a46b3a0aea39c37b1d4c03dc254782670995
Fernando Valle <valleferp@gmail.com>
2020-11-10 12:45:50 +0000

    Create test.java

---
+++ test.java
@@ -0,0 +1,18 @@
+/**
+ *
+ * @author Batman
+ */
+public class SimpleFileReadScanner {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+       
+      System.out.println("Fernando test");
+        
+        
+    }
+    
+}
